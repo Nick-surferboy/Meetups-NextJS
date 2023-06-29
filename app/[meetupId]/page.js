0f128e5aa1072631c7a2f32a5981be9d05f3f54c
Nick <nicolas.dubigny@gmail.com>
2023-06-29 10:32:01 -0400

    Get meetup details

--- app/[meetupId]/page.js
+++ app/[meetupId]/page.js
@@ -1,5 +1,5 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 
 async function DetailsPage({ params }) {
 
@@ -9,24 +9,18 @@ async function DetailsPage({ params }) {
 }
 
 
-
 export async function getData(props) {
   //const data = fetch(DUMMY_MEETUPS)
   const uri = "mongodb+srv://nick:wILMmOy92IRgDikM@cluster0.xembtas.mongodb.net/?retryWrites=true&w=majority";
   const client = await MongoClient.connect(uri);
   const db = client.db();
   const meetupsCollection = db.collection("meetups");
-  console.log(props.meetupId)
 
- // const meetup = await meetupsCollection.find().toArray() ;
+  const meetup = await meetupsCollection.findOne({_id: new ObjectId(props.meetupId)}) ;
   client.close() ;
-  return {
-        id: "m1",
-        title: "A first meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/7/72/Saveh_Bazaar.jpg",
-        address: "Some address 5, 12345",
-        description: " First meetup",
-      } ;
+
+  
+  return meetup ;
 }
 
 
